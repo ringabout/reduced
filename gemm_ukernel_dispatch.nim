3686b33c9f544661d6ce2812a331b334857df74f
flywind <xzsflywind@gmail.com>
2022-01-11 10:35:15 +0800

    best

--- gemm_ukernel_dispatch.nim
+++ gemm_ukernel_dispatch.nim
@@ -5,29 +5,10 @@ type
     cpu_simd: CPUFeatureX86
 
   CPUFeatureX86* = enum
-    x86_Generic,
-    x86_SSE,
-    x86_SSE2,
-    x86_SSE4_1,
-    x86_AVX,
-    x86_AVX_FMA,
-    x86_AVX2,
     x86_AVX512
-
 
 func x86_ukernel*(cpu: CPUFeatureX86, T: typedesc): MicroKernel =
   result.cpu_simd = cpu
-
-type
-  Tiles[T] = ptr TilesObj[T]
-  TilesObj[T] = object
-
-proc newTiles*(
-        ukernel: static MicroKernel,
-        T: typedesc,
-        M, N, K: Natural,
-        ): Tiles[T] =
-  result = createSharedU(TilesObj[T])
 
 {.pragma: x86_type, byCopy, header:"<x86intrin.h>".}
 {.pragma: x86, noDecl, header:"<x86intrin.h>".}
@@ -37,43 +18,27 @@ type
 
 func mm512_setzero_pd(): m512d {.importc: "_mm512_setzero_pd", x86.}
 
-template ukernel_simd_proc(ukernel_name, epilogue_name: NimNode) {.dirty.} =
-  result.add quote do:
-    proc `ukernel_name`[ukernel: static MicroKernel](
-          kc: int,
-          alpha: `T`, packedA, packedB: ptr UncheckedArray[`T`],
-          beta: `T`
-        ) =
-      let AB = ukernel_simd_impl(
-        ukernel, `V`, packedA, packedB, kc,
-        `simd_setZero`
-      )
-
 macro ukernel_generator(
       simd: static CPUFeatureX86,
-      typ: untyped,
-      vectype: untyped,
-      simd_setZero: untyped,
+      vectype: untyped
     ): untyped =
 
-  let T = newIdentNode($typ)
-  let V = newIdentNode($vectype)
-  let epilogue_name = newIdentNode("gebb_ukernel_epilogue_" & $T & "_" & $simd)
   result = newStmtList()
 
   # 2. Generate the microkernels for the general and edge cases
   block:
-    let ukernel_name = newIdentNode("gebb_ukernel_" & $T & "_" & $simd)
-    ukernel_simd_proc(ukernel_name, epilogue_name)
+    let ukernel_name = newIdentNode("gebb_ukernel_" & $float64 & "_" & $simd)
+    result.add quote do:
+      proc `ukernel_name`[ukernel: static MicroKernel]() =
+        let AB = ukernel_simd_impl(ukernel)
 
 macro ukernel_simd_impl(
-      ukernel: static MicroKernel, V: untyped, A, B: untyped, kc: int,
-      simd_setZero: untyped
+      ukernel: static MicroKernel
     ): untyped =
 
   result = newStmtList()
   var
-    rAB = nnkBracket.newTree() # array[MR, array[NbVecs, V]]
+    rAB = nnkBracket.newTree()
 
   var rABi = nnkBracket.newTree()
   rABi.add genSym(nskVar, "AB" & $0 & "__" & $0)
@@ -83,49 +48,31 @@ macro ukernel_simd_impl(
   var declBody = newStmtList()
   let ab = rAB[0][0]
   declBody.add quote do:
-    var `ab` = `simd_setZero`()
+    var `ab` = mm512_setzero_pd()
 
   ## Assemble:
   result = quote do:
     `declBody`
     `rAB`
 
-
 {.localpassC:"-mavx512f -mavx512dq".}
 
 ukernel_generator(
     x86_AVX512,
-    typ = float64,
-    vectype = m512d,
-    simd_setZero = mm512_setzero_pd,
+    vectype = m512d
   )
 
 {.experimental: "dynamicBindSym".}
 
-macro dispatch_general(
+macro dispatch_general*(
     ukernel: static MicroKernel,
-    kc: int,
-    alpha: typed, packedA, packedB: ptr UncheckedArray[typed],
-    beta: typed
   ): untyped =
   let simd = ukernel.cpu_simd
 
   result = newStmtList()
 
   # 2. Dispatch according to type and SIMD support
-  let symT = getTypeInst(alpha)
   let simdTag = $simd
-  let ukernel_name = bindSym("gebb_ukernel_" & $symT & "_" & simdTag)
+  let ukernel_name = bindSym("gebb_ukernel_" & $float64 & "_" & simdTag)
   result.add quote do:
-    `ukernel_name`[ukernel]( # Hack: ukernel is generic from the calling proc
-      `kc`,
-      `alpha`, `packedA`, `packedB`,
-      `beta`
-    )
-
-proc gebb_ukernel*[T; ukernel: static MicroKernel](
-      kc: int,
-      alpha: T, packedA, packedB: ptr UncheckedArray[T],
-      beta: T
-    ){.inline.} =
-  ukernel.dispatch_general(kc, alpha, packedA, packedB, beta)
+    `ukernel_name`[ukernel]()
--- test.nim
+++ test.nim
@@ -3,9 +3,4 @@ import
   gemm_ukernel_dispatch
 
 const ukernel = x86_AVX512.x86_ukernel(float64)
-let tiles = ukernel.newTiles(float64, 1, 2, 3)
-gebb_ukernel[float64, ukernel](                    # GEBB microkernel + epilogue
-            1,                                  #   C[ic+ir:ic+ir+mr, jc+jr:jc+jr+nr] =
-      2, nil, nil,                 #    αA[ic+ir:ic+ir+mr, pc:pc+kc] *
-      3                                #     B[pc:pc+kc, jc+jr:jc+jr+nr] +
-    )                                            #    βC[ic:ic+mc, jc:jc+nc]
+dispatch_general(ukernel)
