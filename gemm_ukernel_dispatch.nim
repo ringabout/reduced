87781942ce62f2b0826e6a502aac7f49cfe89c24
flywind <xzsflywind@gmail.com>
2022-01-11 11:02:21 +0800

    smaller

--- gemm_ukernel_dispatch.nim
+++ gemm_ukernel_dispatch.nim
@@ -7,6 +7,8 @@ type
   CPUFeatureX86* = enum
     x86_AVX512
 
+{.localpassC:"-mavx512f -mavx512dq".}
+
 func x86_ukernel*(cpu: CPUFeatureX86, T: typedesc): MicroKernel =
   result.cpu_simd = cpu
 
@@ -18,61 +20,39 @@ type
 
 func mm512_setzero_pd(): m512d {.importc: "_mm512_setzero_pd", x86.}
 
-macro ukernel_generator(
-      simd: static CPUFeatureX86,
-      vectype: untyped
-    ): untyped =
-
+macro ukernel_generator(simd: static CPUFeatureX86): untyped =
   result = newStmtList()
-
-  # 2. Generate the microkernels for the general and edge cases
   block:
     let ukernel_name = newIdentNode("gebb_ukernel_" & $float64 & "_" & $simd)
     result.add quote do:
       proc `ukernel_name`[ukernel: static MicroKernel]() =
         let AB = ukernel_simd_impl(ukernel)
 
-macro ukernel_simd_impl(
-      ukernel: static MicroKernel
-    ): untyped =
-
+macro ukernel_simd_impl(ukernel: static MicroKernel): untyped =
   result = newStmtList()
-  var
-    rAB = nnkBracket.newTree()
-
+  var rAB = nnkBracket.newTree()
   var rABi = nnkBracket.newTree()
   rABi.add genSym(nskVar, "AB" & $0 & "__" & $0)
   rAB.add rABi
 
-  ## Declare
   var declBody = newStmtList()
   let ab = rAB[0][0]
   declBody.add quote do:
     var `ab` = mm512_setzero_pd()
 
-  ## Assemble:
   result = quote do:
     `declBody`
     `rAB`
 
-{.localpassC:"-mavx512f -mavx512dq".}
-
-ukernel_generator(
-    x86_AVX512,
-    vectype = m512d
-  )
+ukernel_generator(x86_AVX512)
 
 {.experimental: "dynamicBindSym".}
 
 macro dispatch_general*(
     ukernel: static MicroKernel,
   ): untyped =
-  let simd = ukernel.cpu_simd
-
   result = newStmtList()
-
-  # 2. Dispatch according to type and SIMD support
-  let simdTag = $simd
+  let simdTag = $ukernel.cpu_simd
   let ukernel_name = bindSym("gebb_ukernel_" & $float64 & "_" & simdTag)
   result.add quote do:
     `ukernel_name`[ukernel]()
